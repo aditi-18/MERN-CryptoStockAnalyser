9b708682779a93758c52421184c25d97180aba25
Ayush Sahai <sahai.a@northeastern.edu>
2021-05-27 20:08:35 -0400

    IssueTable Passing Data to IssueRow

--- src/App.jsx
+++ src/App.jsx
@@ -17,9 +17,23 @@ class IssueRow extends React.Component {
   }	
 }
 class IssueTable extends React.Component {
-  render(){
-    return(
-      <div>This is a placeholder for a table of issues.</div>
+ render() {	
+    const rowStyle = {border: "1px solid silver", padding: 4};	
+    return (	
+      <table style={{borderCollapse: "collapse"}}>	
+        <thead>	
+          <tr>	
+            <th style={rowStyle}>ID</th>	
+            <th style={rowStyle}>Title</th>	
+          </tr>	
+        </thead>	
+        <tbody>	
+          <IssueRow rowStyle={rowStyle} issue_id={1}	
+            issue_title="Error in console when clicking Add" />	
+          <IssueRow rowStyle={rowStyle} issue_id={2}	
+            issue_title="Missing bottom border on panel" />	
+        </tbody>	
+      </table>
     );
   }
 }
