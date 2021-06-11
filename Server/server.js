be9c282dadf641ee3c49ec7ba6b04254def6f2a0
Ayush Sahai <sahai.a@northeastern.edu>
2021-06-11 11:04:21 -0400

    LIST API integration and changes made to server file

--- Server/server.js
+++ Server/server.js
@@ -2,9 +2,22 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 let aboutMessage = "Issue Tracker API v1.0";
 const fs = require('fs');
+const issuesDB = [
+{	
+    id: 1, status: 'New', owner: 'Ayush', effort: 5,	
+    created: new Date('2021-05-27'), due: undefined,	
+    title: 'Error in console when clicking Add',	
+  },	
+  {	
+    id: 2, status: 'Assigned', owner: 'Ani', effort: 14,	
+    created: new Date('2021-05-16'), due: new Date('2018-08-30'),	
+    title: 'Missing bottom border on panel',	
+  },	
+];
 const resolvers = {
   Query: {
   about: () => aboutMessage,
+  issueList,
   },
   Mutation: {
   setAboutMessage,
@@ -12,6 +25,9 @@ const resolvers = {
 };
 function setAboutMessage(_, {message}) {
   return aboutMessage = message;
+}
+function issueList() {
+return issuesDB;
 }
 const server = new ApolloServer({
 typeDefs: fs.readFileSync('./server/schema.graphql', 'utf-8'),
