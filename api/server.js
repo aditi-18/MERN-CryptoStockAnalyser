190cc26ffc5b8dced8b7354a4b148d5e76b64a44
Ayush Sahai <sahai.a@northeastern.edu>
2021-06-25 14:18:43 -0400

    Architectural changes

--- api/server.js
+++ api/server.js
@@ -77,7 +77,7 @@ console.log('Connected to MongoDB at', url);
 db = client.db();
 }
 const server = new ApolloServer({
-  typeDefs: fs.readFileSync('./server/schema.graphql', 'utf-8'),
+  typeDefs: fs.readFileSync('schema.graphql', 'utf-8'),
   resolvers,
   formatError: error => {
     console.log(error);
@@ -85,13 +85,12 @@ const server = new ApolloServer({
   },
 });
 const app = express();
-app.use(express.static('public'));
 server.applyMiddleware({app, path: '/graphql'});
 (async function () {
   try {
     await connectToDb();
     app.listen(3000, function () {
-      console.log('App started on port 3000');
+      console.log('API server started on port 3000');
     });
   } catch (err) {
     console.log('ERROR:', err);
