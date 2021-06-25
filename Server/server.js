be048d172ea19a4113f1be7d158909c42ae2e5d0
Ayush Sahai <sahai.a@northeastern.edu>
2021-06-25 10:10:25 -0400

    write to mongo

--- Server/server.js
+++ Server/server.js
@@ -7,18 +7,14 @@ const url = 'mongodb://localhost/issuetracker';
 let aboutMessage = "Issue Tracker API v1.0";
 let db;
 const fs = require('fs');
-const issuesDB = [
-{	
-    id: 1, status: 'New', owner: 'Ayush', effort: 5,	
-    created: new Date('2021-05-27'), due: undefined,	
-    title: 'Error in console when clicking Add',	
-  },	
-  {	
-    id: 2, status: 'Assigned', owner: 'Ani', effort: 14,	
-    created: new Date('2021-05-16'), due: new Date('2018-08-30'),	
-    title: 'Missing bottom border on panel',	
-  },	
-];
+async function getNextSequence(name) {
+const result = await db.collection('counters').findOneAndUpdate(
+{ _id: name },
+{ $inc: { current: 1 } },
+{ returnOriginal: false },
+);
+return result.value.current;
+}
 const GraphQLDate = new GraphQLScalarType({
   name:'GraphQLDate',
   description: 'A Date() type in GraphQL as a scalar',
@@ -61,12 +57,14 @@ function issueValidate(issue) {
     throw new UserInputError('Invalid input(s)', { errors });
   }
 }
-function issueAdd(_, { issue }) {
+async function issueAdd(_, { issue }) {
   issueValidate(issue);
   issue.created = new Date();
-  issue.id = issuesDB.length + 1;
-  issuesDB.push(issue);
-  return issue;
+ issue.id = await getNextSequence('issues');
+  const result = await db.collection('issues').insertOne(issue);
+ const savedIssue = await db.collection('issues')
+.findOne({ _id: result.insertedId });
+return savedIssue;
 }
 async function issueList() {
 const issues = await db.collection('issues').find({}).toArray();
