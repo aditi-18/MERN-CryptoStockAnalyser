4ddc954f30f32f29b0f67ae58c5380c8c6596f45
Ayush Sahai <sahai.a@northeastern.edu>
2021-06-29 19:08:44 -0400

    minor edits

--- api/server.js
+++ api/server.js
@@ -7,7 +7,7 @@ const { Kind } = require('graphql/language');
 const { MongoClient } = require('mongodb');
 
 const url = process.env.DB_URL || 'mongodb://localhost/issuetracker';
-
+const port = process.env.API_SERVER_PORT || 3000;
 let db;
 
 let aboutMessage = 'Issue Tracker API v1.0';
@@ -109,7 +109,6 @@ const enableCors = (process.env.ENABLE_CORS || 'true') === 'true';
 console.log('CORS setting:', enableCors);
 server.applyMiddleware({ app, path: '/graphql', cors: enableCors });
 
-const port = process.env.API_SERVER_PORT || 3000;
 
 (async function start() {
   try {
