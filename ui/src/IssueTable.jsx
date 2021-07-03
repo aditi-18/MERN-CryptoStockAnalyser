ff8acffd338116d8c6a33ffa1510ffc5c77e71f1
Ayush Sahai <sahai.a@northeastern.edu>
2021-07-03 07:54:22 -0400

    libraries bundle added fully

--- ui/src/IssueTable.jsx
+++ ui/src/IssueTable.jsx
@@ -1,4 +1,3 @@
-  
 import React from 'react';
 function IssueRow({ issue }) {
   return (
--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-
 module.exports = {
   mode: 'development',
   entry: { app: './src/App.jsx' },
