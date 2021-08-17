4a41df8719d4c95b48beba11bc85a8af0e9e66f9
janvi dankhara <dankhara.j@northeastern.edu>
2021-08-17 19:02:03 -0400

    Fixed stock search page

--- ui/src/StockSearch.jsx
+++ ui/src/StockSearch.jsx
@@ -13,90 +13,53 @@ class StockSearch extends Component {
     this.state = {
 
       name: 'React',
-
       showHideDemo1: true,
-      showHideDemo2: false,
-      showHideDemo3: false,
-      showHideDemo4: false,
-      showHideDemo5: false,
-      showHideDemo6: false,
 
     };
 
     this.hideComponent = this.hideComponent.bind(this);
   }
 
+ a = {
+
+    showHideDemo1: false,
+    showHideDemo2: false,
+    showHideDemo3: false,
+    showHideDemo4: false,
+    showHideDemo5: false,
+    showHideDemo6: false,
+
+  }
   hideComponent(name) {
-    console.log(name);
 
     switch (name) {
-      case 'showHideDemo1':
+      case 'IBM':
+        this.setState({...this.a, showHideDemo1: true });
+        break;
 
-        this.setState({ showHideDemo1: true });
-        this.setState({ showHideDemo2: false });
-        this.setState({ showHideDemo3: false });
-        this.setState({ showHideDemo4: false });
-        this.setState({ showHideDemo5: false });
-        this.setState({ showHideDemo6: false });
+      case 'Shopify':
+        this.setState({...this.a, showHideDemo2: true });
+        break;
+
+      case 'Dai':
+        this.setState({...this.a, showHideDemo3: true });
+        break;
+
+      case 'GreenPower':
+
+        this.setState({ ...this.a,showHideDemo4: true });
+
+        break;
+      case 'Saic':
+this.setState({...this.a, showHideDemo5: true });
 
         break;
 
-      case 'showHideDemo2':
+      case 'Tesco':
 
-        this.setState({ showHideDemo1: false });
-        this.setState({ showHideDemo2: true });
-        this.setState({ showHideDemo3: false });
-        this.setState({ showHideDemo4: false });
-        this.setState({ showHideDemo5: false });
-        this.setState({ showHideDemo6: false });
+        this.setState({...this.a, showHideDemo6: true });
 
         break;
-
-      case 'showHideDemo3':
-
-        this.setState({ showHideDemo1: false });
-        this.setState({ showHideDemo2: false });
-        this.setState({ showHideDemo3: true });
-        this.setState({ showHideDemo4: false });
-        this.setState({ showHideDemo5: false });
-        this.setState({ showHideDemo6: false });
-
-        break;
-
-      case 'showHideDemo4':
-
-        this.setState({ showHideDemo1: false });
-        this.setState({ showHideDemo2: false });
-        this.setState({ showHideDemo3: false });
-        this.setState({ showHideDemo4: true });
-        this.setState({ showHideDemo5: false });
-        this.setState({ showHideDemo6: false });
-
-        break;
-      case 'showHideDemo5':
-
-        this.setState({ showHideDemo1: false });
-        this.setState({ showHideDemo2: false });
-        this.setState({ showHideDemo3: false });
-        this.setState({ showHideDemo4: false });
-        this.setState({ showHideDemo5: true });
-        this.setState({ showHideDemo6: false });
-
-        break;
-
-      case 'showHideDemo6':
-
-        this.setState({ showHideDemo1: false });
-        this.setState({ showHideDemo2: false });
-        this.setState({ showHideDemo3: false });
-        this.setState({ showHideDemo4: false });
-        this.setState({ showHideDemo5: false });
-        this.setState({ showHideDemo6: true });
-
-        break;
-      default:
-
-        null;
     }
   }
 
@@ -112,19 +75,24 @@ class StockSearch extends Component {
           <form>
             <label>
               Pick your favorite:
-              <select>
-                <option value="IBM" onClick={() => this.hideComponent('showHideDemo1')}>IBM</option>
-                <option value="Shopify" onClick={() => this.hideComponent('showHideDemo2')}>Shopify</option>
-                <option value="Dai" onClick={() => this.hideComponent('showHideDemo3')}>Dai</option>
-                <option value="GreenPower" onClick={() => this.hideComponent('showHideDemo4')}>GreenPower</option>
-                <option value="Saic" onClick={() => this.hideComponent('showHideDemo5')}>Saic</option>
-                <option value="Tesco" onClick={() => this.hideComponent('showHideDemo6')}>Tesco</option>
+              <select onChange={(e)=>{
+                this.hideComponent(e.target.value)
+                }
+                }
+                  >
+                <option value="IBM" >IBM</option>
+                <option value="Shopify" >Shopify</option>
+                <option value="Dai" >Dai</option>
+                <option value="GreenPower" >GreenPower</option>
+                <option value="Saic" >Saic</option>
+                <option value="Tesco" >Tesco</option>
               </select>
 
             </label>
 
           </form>
         </div>
+
 
         {showHideDemo1 && <IBM />}
 
